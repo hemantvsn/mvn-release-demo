195d2fa8ab912e8c6370dd8ac9515c6523786f5d
Hemant Nagpure <hemant.nagpure@concirrus.com>
2020-10-26 19:47:31 +0530

    first commit

---
+++ src/main/java/com/hemant/mvnreleasedemo/MvnReleaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hemant.mvnreleasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnReleaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnReleaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemant/mvnreleasedemo/MvnReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemant.mvnreleasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnReleaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
